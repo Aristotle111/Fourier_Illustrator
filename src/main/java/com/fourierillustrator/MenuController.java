5657d3d47156c2c33d94cf94a0d37a24de70ec8f
Aristotle <aristotleprovatas@gmail.com>
2025-11-30 17:55:30 -0500

    made changes to main menu and added button borders when user hovers over a button to indicate selection

--- src/main/java/com/fourierillustrator/MenuController.java
+++ src/main/java/com/fourierillustrator/MenuController.java
@@ -4,15 +4,19 @@ import java.io.IOException;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 
 public class MenuController {
 
     @FXML private Button scene1Button;
     @FXML private Button scene2Button;
+    @FXML private ImageView button1Border;
+    @FXML private ImageView button2Border;
 
     @FXML
     private void initialize() {
-
+        button1Border.setVisible(false);
+        button2Border.setVisible(false);
     }
 
     @FXML
@@ -23,5 +27,25 @@ public class MenuController {
     @FXML
     private void openScene2() throws IOException {
         Main.setScene2FromMenu();
+    }
+
+    @FXML
+    private void button1Hovered() {
+        button1Border.setVisible(true);
+    }
+
+    @FXML
+    private void button1Exited() {
+        button1Border.setVisible(false);
+    }
+
+    @FXML
+    private void button2Hovered() {
+        button2Border.setVisible(true);
+    }
+
+    @FXML
+    private void button2Exited() {
+        button2Border.setVisible(false);
     }
 }
